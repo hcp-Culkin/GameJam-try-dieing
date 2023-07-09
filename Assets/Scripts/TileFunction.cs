225d3e372bdab2309865d7563c2f9d8c34af4130
Culkin <67741459+hcp-Culkin@users.noreply.github.com>
2023-07-09 16:37:54 +0530

    Road prefabs

--- Assets/Scripts/TileFunction.cs
+++ Assets/Scripts/TileFunction.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TileFunction : MonoBehaviour
 {
     // Start is called before the first frame update
 
+
+
+    public Sprite[] SpritesToRandom;
+
+    private void Start()
+    {
+        GetComponent<Image>().sprite = SpritesToRandom[Random.Range(0, SpritesToRandom.Length)];
+
+
+    }
 
     public void OnItemPlacer()
     {
